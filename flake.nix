c7eed25ca84ac1eb5add10ac3c4a47dc3ed76155
Stephen Huan <stephen.huan@cgdct.moe>
2024-06-30 15:45:37 -0400

    Fix flake.nix by removing crypton-connection pin

--- flake.nix
+++ flake.nix
@@ -7,18 +7,6 @@
   outputs = inputs:
     let
       overlay = final: prev: {
-        haskell = prev.haskell // {
-          packageOverrides = hfinal: hprev:
-            prev.haskell.packageOverrides hfinal hprev // {
-              crypton-connection = final.fetchFromGitHub {
-                owner = "kazu-yamamoto";
-                repo = "crypton-connection";
-                rev = "5c064b911e7327a4d399fd9dd057663d0d0fb256";
-                sha256 = "00j1nf9glbz0cnzd84vp08j9ybzjbm3b6gcamlqxxcjb31kllz4b";
-              };
-              pandoc = hfinal.callCabal2nix "pandoc" ./. { };
-            };
-        };
         pandoc = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.pandoc;
       };
       perSystem = system:
